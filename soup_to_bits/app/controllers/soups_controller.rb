b26f5c39124bc8a13fb7664f676df1aa7131b47f
Wenjing Liu <liuwenjingchina@gmail.com>
2015-10-17 20:08:27 -0400

    Finalize soup to bits [Description] Finish soup to bits first series

--- soup_to_bits/app/controllers/soups_controller.rb
+++ soup_to_bits/app/controllers/soups_controller.rb
@@ -1,5 +1,50 @@
 class SoupsController < ApplicationController
+  before_action :fetch_soup, only: [:show, :edit, :update, :destroy, :toggle_featured]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json {render json: @soup}
+    end
+  end
+  
   def index
+    @soups = Soup.all
+    
+    respond_to do |format|
+      format.json {render json: @soups}
+    end
+  end
+  
+  def new
     
   end
+  
+  def edit
+  
+  end
+  
+  def udpate
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  def fetch_soup
+    @soup = Soup.find(params[:id])
+  end
+  
+  def toggle_featured
+    @soup.toggle!(:featured)
+    flash[:notice] = "Successfully changed the featured flag."
+    redirect_to @soup
+  end
+  
+  def secret_soups
+    session[:vip] = true 
+    redirect_to root_path
+  end
 end
+
